09659de33d5a995948f5706e1a8a75fe536a8cbc
heesungkim010 <heesungkim010@gmail.com>
2020-05-29 21:47:35 +0900

    finished developing get_string().

--- kr_wordcloud2.py
+++ kr_wordcloud2.py
@@ -1,6 +1,5 @@
 from konlpy.tag import Hannaum
 
-
 def listToString(list1):
     str=""
     return (str.join(list1))
@@ -9,3 +8,8 @@ def get_string():
     f = open("파일명.txt", "r", encoding="utf-8") 
     sample = f.read()
     f.close()
+    h = Hannaum()
+    list_nouns = h.nouns(sample) #get list of nouns from sample
+    return listToString(list_nouns) #get string of list_nouns
+
+tags = get_string()  # tags : string of list_nouns
