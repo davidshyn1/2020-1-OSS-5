2ea38cfad6ab5ddc06f35742316cd86a4fc2d3d9
davidshyn1 <59733867+davidshyn1@users.noreply.github.com>
2020-05-30 15:49:19 +0900

    Update kr_wordcloud2.py

--- kr_wordcloud2.py
+++ kr_wordcloud2.py
@@ -1,15 +1,27 @@
-from konlpy.tag import Hannaum
+import os
+from os import path
+from konlpy.tag import Hannanum
+from wordcloud import WordCloud
+
+# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+
+font_path = d + '/word_cloud/examples/fonts/NotoSansKR/NotoSansKR-Black.otf'
 
 def listToString(list1):
-    str=""
+    str=" " #distinguish by 'space'
     return (str.join(list1))
 
-def get_string():
-    f = open("파일명.txt", "r", encoding="utf-8") 
+def get_string(path):
+    f = open(path, "r", encoding="utf-8")
     sample = f.read()
     f.close()
-    h = Hannaum()
+    h = Hannanum()
     list_nouns = h.nouns(sample) #get list of nouns from sample
     return listToString(list_nouns) #get string of list_nouns
 
-tags = get_string()  # tags : string of list_nouns
+path = "text파일.txt"
+tags = get_string(path)  # tags : string of list_nouns
+wc = WordCloud(font_path=font_path, background_color="white",
+               max_font_size=100, random_state=42, width=1000, height=860, margin=2)
+
