993435e0b3268949dc87980e666d065ed5626e9d
davidshyn1 <59733867+davidshyn1@users.noreply.github.com>
2020-06-05 13:57:04 +0900

    Update kr_wordcloud2.py

--- kr_wordcloud2.py
+++ kr_wordcloud2.py
@@ -1,18 +1,19 @@
 import os
 
 from os import path
-from wordcloud import WordCloud
 from konlpy.tag import Hannanum
-
+from wordcloud import WordCloud
 
 # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+
 
 #get the path of Korean_fonts otf file
 font_path = d + '/word_cloud/examples/fonts/NotoSansKR/NotoSansKR-Black.otf'
 
 def listToString(list1):
     str=" " #distinguish nouns by 'space'
+
     return (str.join(list1))
 
 def get_string(path):
@@ -23,8 +24,14 @@ def get_string(path):
     list_nouns = h.nouns(sample) #get list of nouns from sample
     return listToString(list_nouns) #get string of list_nouns
 
-path = d + "텍스트파일.txt"
+path = d + '/word_cloud/kor_text/2020.05.28.기사1.txt'
 tags = get_string(path)  # tags : string of list_nouns
+wc = WordCloud(font_path=font_path, background_color="white",collocations=False,
+               max_font_size=100, random_state=42, width=1000, height=860, margin=2)
 
-wc = WordCloud(font_path=font_path, background_color="white")
-draw_wc = wc.generate(tags)
+#display the generated image
+wordcloud = wc.generate(tags)
+import matplotlib.pyplot as plt
+plt.imshow(wordcloud, interpolation ='bilinear')
+plt.axis("off")
+plt.show()
